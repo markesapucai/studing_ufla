56efc25c1aecc281beba4435fa41888d39032ca2
Mark Leite <marktobusiness113@gmail.com>
2024-10-21 15:11:24 -0300

    true

--- c++/learning/rep.cpp
+++ c++/learning/rep.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
-int calcula(int a) {
-    int multi = 1;
-    for(int x = a; x > 1; x--) {
-        multi *= x;
-    }
-    return multi;
-}
 
 int main() {
-    int a;
-    cout << "Digite um número inteiro positivo" << endl;
-    cin >> a;
-    int resul = calcula(a);
-    int resulBibli = tgamma(a + 1);
-    cout << "O fatorial de " << a << " e " << resul << " PELO CALCULO" << endl;
-    cout << "O fatorial de " << a << " e " << resulBibli << " PELA BIBLIOTECA" << endl;
+    int x = 0, y = 8;
+
+    while(x < 4 or y > 5) {
+        x++;
+        y++;
+        y = y - x;
+    }
+
+    cout << x + y << endl;
+
     return 0;
 }
